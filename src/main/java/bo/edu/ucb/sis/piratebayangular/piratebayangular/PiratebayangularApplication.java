67155f4ab6883afbb8a82ed75cb47a7f34807f3b
LizbethVera <lizveraduran@gmail.com>
2020-06-15 02:04:09 -0400

    Eprezando proyecto

---
+++ src/main/java/bo/edu/ucb/sis/piratebayangular/piratebayangular/PiratebayangularApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.sis.piratebayangular.piratebayangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiratebayangularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiratebayangularApplication.class, args);
+	}
+
+}
