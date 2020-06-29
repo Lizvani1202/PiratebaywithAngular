85147156ce981c869482f6ece64cc53230409383
LizbethVera <lizveraduran@gmail.com>
2020-06-28 23:25:01 -0400

    Interfaz de Orden

---
+++ src/main/java/bo/edu/ucb/sis/piratebayangular/piratebayangular/orden/service/iOrdenService.java
@@ -0,0 +1,18 @@
+package bo.edu.ucb.sis.piratebayangular.piratebayangular.orden.service;
+
+import bo.edu.ucb.sis.piratebayangular.piratebayangular.orden.Entity.Orden;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface iOrdenService {
+    public List<Orden> findAll();
+    public Orden findOrden(Orden orden);
+
+    public void deleteOrder(Orden orden);
+    public Orden updateOrden(Orden orden);
+    public Optional<Orden> findOrdenByIdOrden(Long idOrden);
+    public void deleteOrden(Long id);
+    public Orden findByIdOrden(Long id);
+    public Orden findByIdOrdenSQL(Long id);
+}
